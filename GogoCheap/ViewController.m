2b2c236f349bbd79a27f00efb3f842cd80ce32cf
Wayne <waynehartman@gmail.com>
2014-07-30 11:36:48 -0600

    Initial commit

--- GogoCheap/ViewController.m
+++ GogoCheap/ViewController.m
@@ -8,20 +8,71 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UIWebViewDelegate>
+
+@property (strong, nonatomic) IBOutlet UIWebView *webView;
+@property (strong, nonatomic) IBOutlet UIBarButtonItem *backButton;
+@property (strong, nonatomic) IBOutlet UIBarButtonItem *forwardButton;
+@property (strong, nonatomic) IBOutlet UIView *activityView;
 
 @end
+
+#define USER_AGENT @"Mozilla/5.0 (iPhone; U; CPU iPhone OS 4_1 like Mac OS X; en-us) AppleWebKit/532.9 (KHTML, like Gecko) Version/4.0.5 Mobile/8B117 Safari/6531.22.7"
 
 @implementation ViewController
             
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.webView.scrollView.contentInset = UIEdgeInsetsMake([UIApplication sharedApplication].statusBarFrame.size.height, 0.0f, 0.0f, 0.0f);
+    [self.webView loadRequest:[self request]];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (NSURLRequest *)request {
+    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:@"http://gogoinflight.com"]];
+    [request setValue:USER_AGENT forHTTPHeaderField:@"User-Agent"];
+    return request;
 }
 
+#pragma mark - Other Instance Methods
+
+- (void)updateButtonState {
+    self.backButton.enabled = self.webView.canGoBack;
+    self.forwardButton.enabled = self.webView.canGoForward;
+}
+
+#pragma mark - Actions
+
+
+- (IBAction)didSelectBackButton:(id)sender {
+    [self.webView goBack];
+}
+
+- (IBAction)didSelectForwardButton:(id)sender {
+    [self.webView goForward];
+}
+
+#pragma mark - UIWebViewDelegate
+
+- (void)webView:(UIWebView *)webView didFailLoadWithError:(NSError *)error {
+    if (error.code != NSURLErrorCancelled) {
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:NSLocalizedString(@"Error", nil) message:error.description delegate:nil cancelButtonTitle:NSLocalizedString(@"OK", nil) otherButtonTitles:nil];
+        [alert show];
+    }
+
+    [self updateButtonState];
+    self.activityView.hidden = YES;
+}
+
+- (void)webViewDidFinishLoad:(UIWebView *)webView {
+    [self updateButtonState];
+    self.activityView.hidden = YES;
+}
+
+- (void)webViewDidStartLoad:(UIWebView *)webView {
+    self.activityView.hidden = NO;
+}
+
+- (BOOL)webView:(UIWebView *)webView shouldStartLoadWithRequest:(NSURLRequest *)request navigationType:(UIWebViewNavigationType)navigationType {
+    return YES;
+}
 @end
